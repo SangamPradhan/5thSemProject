30678432ed61e56352356eb29d6aa3ca92609aff
SangamPradhan <142468434+SangamPradhan@users.noreply.github.com>
2024-11-28 07:06:14 +0545

    created admin layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -8,9 +9,14 @@ Route::get('/', function () {
     return Inertia::render('Home');
 });
 
+Route::get('/admin', function () {
+    return Inertia::render('Admin');
+});
+
 
 Route::get('/contactpage', [HomeController::class, 'contactpage']);
 Route::get('/aboutus', [HomeController::class, 'aboutus']);
 Route::get('/gallery', [HomeController::class, 'gallery']);
 Route::get('/event', [HomeController::class, 'event']);
 Route::get('/project', [HomeController::class, 'project']);
+
